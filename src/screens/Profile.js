569cc4b0ea4f09941aa97c4df6bc1f35f0cdfa61
Qinglin,Xia <xiaqinglinv5@gmail.com>
2023-10-12 16:50:59 +0800

    modify profile page for artists users

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,20 +1,31 @@
 import React,{useEffect, useState} from 'react'
 import '../Styles/Profile.css'
 import { getAuth,   onAuthStateChanged } from 'firebase/auth'
-  import {collection , where, query, getDocs, getDoc, doc, orderBy,  } from "firebase/firestore";
- import { db } from '../firebase/config';
+import {collection , where, query, getDocs, getDoc, doc, orderBy,  } from "firebase/firestore";
+import { db } from '../firebase/config';
 import CustomerOrder from '../pages/Customer/CustomerOrder';
-  
-      const Profile =()=>{  
-      const [prof, setProf] = useState([]);
-  const auth = getAuth();  
- 
+import ArtistOrder from '../pages/Artist/ArtistOrders';
+
+import { IoChevronDownCircle } from 'react-icons/io5';
+import ArtistsFetchCol from "../customHooks/ArtistsFetchCol";
+
+const Profile =()=>{  
+const [prof, setProf] = useState([]);
+const auth = getAuth();  
+let isArtist = false
 
  useEffect(() => {
   onAuthStateChanged(auth, (user)=>{
     if (user){
       const uid =  user.uid;
-      console.log(uid)
+      const {artist} = ArtistsFetchCol('profileUpdate')
+      console.log(artist)
+      artist.forEach(artist=>{
+        if (uid == artist.id){
+          isArtist = true  
+          console.log("isArtist:" , isArtist)
+        }
+      })
       const artistDocRef = doc(db, 'users', uid);
       const fetchArtist = async () => {
         const docSnap = await getDoc(artistDocRef);
@@ -53,7 +64,11 @@ import CustomerOrder from '../pages/Customer/CustomerOrder';
 )
   }  
     </div>  
-      <CustomerOrder/>
+    {isArtist ? (
+        <CustomerOrder/>
+      ) : (
+        <ArtistOrder />
+      )}
     </div>           
     </div>       
     </div>      

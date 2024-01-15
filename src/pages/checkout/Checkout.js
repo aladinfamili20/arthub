7b629c0986864815968938c95a870d0b9e8de65c
aladinfamili20 <aladinfamili20@gmail.com>
2024-01-15 16:04:58 -0600

    heroku 1

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -40,7 +40,7 @@ const Checkout = () => {
     // http://localhost:4242/create-payment-intent
     // Create PaymentIntent as soon as the page loads
     // const url = 'https://art-hub.us/create-payment-intent'
-    fetch('https://arthub-psi.vercel.app/create-payment-intent', {
+    fetch('https://arthub-d538edc66138.herokuapp.com/create-payment-intent', {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
